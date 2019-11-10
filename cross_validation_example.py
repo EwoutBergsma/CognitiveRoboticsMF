90c8f324b0f200936ceba3986b3f9294b33eb849
gitaar9 <thijs.eker@gmail.com>
2019-11-10 15:47:30 +0100

    Added an example file which uses the new cross-validation Added a file for generating features from images using the VGG19 model Updated the random forest hyperparameter search script Added a bunch of comments

---
+++ cross_validation_example.py
@@ -0,0 +1,15 @@
+from skgarden import MondrianForestClassifier
+from sklearn.model_selection import cross_val_score
+
+from load_dataset import load_vfh_data
+
+# Load_vfh_data return the data, labels(targets) and a generator that can be used to fit with 10-fold cross-validation
+data, labels, cv_generator = load_vfh_data()
+print(data.shape, labels.shape)
+
+# Create model
+model = MondrianForestClassifier(n_estimators=20)
+# Get the scores
+scores = cross_val_score(model, data, labels, cv=cv_generator)
+print(scores)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
--- rf_hyperparam_search.py
+++ rf_hyperparam_search.py
@@ -1,18 +1,16 @@
 from pprint import pprint
 
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
 from load_dataset import load_vfh_data
 
 # Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start=100, stop=400, num=4)]
+n_estimators = [300, 400]
 # Number of features to consider at every split
-max_features = ['auto', 'sqrt']
+max_features = ['auto'] # , 'sqrt'
 # Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 50, num=5)]
-max_depth.append(None)
+max_depth = [40, 50, None]  # int(x) for x in np.linspace(10, 50, num=5)]
 # Minimum number of samples required to split a node
 min_samples_split = [2, 5, 10]
 # Minimum number of samples required at each leaf node
@@ -30,15 +28,25 @@ grid = {'n_estimators': n_estimators,
 
 pprint(grid)
 
+# Load the data, labels and cv idxs generator
+data, labels, cv_generator = load_vfh_data()
+
+# Initialize a classifier
 rf = RandomForestClassifier()
 
-data, labels, cv_generator = load_vfh_data()
+# The RandomizedSearchCV will try out n_iter random combinations of the supplied grid, n_jobs specifies the amount of
+# workers used, don't run 20 workers on a your laptop since this requires tons of RAM.
 rf_random_search = RandomizedSearchCV(estimator=rf, param_distributions=grid, n_iter=100, cv=cv_generator, verbose=2,
-                                      random_state=42, n_jobs=4)
+                                      n_jobs=20)
 # rf_grid_search = GridSearchCV(estimator=rf, param_grid=grid, verbose=2, cv=cv_generator, n_jobs=6)
 
+# Run the random hyperparameter search
 rf_random_search.fit(data, labels)
-print(rf_random_search.cv_results_)
-print(rf_random_search.best_params_)
-print(rf_random_search.best_score_)
+
+# Print all scores for the different setups tried
 print(rf_random_search.cv_results_['mean_test_score'])
+# Print the params with the best result
+print(rf_random_search.best_params_)
+# Print the best result
+print(rf_random_search.best_score_)
+
