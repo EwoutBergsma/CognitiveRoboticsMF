8c394899bd2fa00e18b657ed227829b3a9d3bef3
gitaar9 <thijs.eker@gmail.com>
2019-11-12 16:29:25 +0100

    save amount of samples used

--- cross_validation_for_al_mf.py
+++ cross_validation_for_al_mf.py
@@ -11,11 +11,14 @@ num_trees_depth_data = 22
 data, labels, cv_generator = load_vfh_data()
 
 scores = []
+samples_used = []
 for training_idxs, validation_idxs in cv_generator:
     mf = MondrianForestClassifierWithALStrategy(n_estimators=22)
-    mf.fit_using_al_strategy_thres(data[training_idxs], labels[training_idxs], np.array(range(51)), 300, 0.5)
+    samples_used.append(mf.fit_using_al_strategy_thres(data[training_idxs], labels[training_idxs], np.array(range(51)),
+                                                       300, 0.5))
     scores.append(mf.score(np.array(data[validation_idxs, :]), np.array(labels[validation_idxs])))
-    print(scores[-1])
+    print(scores[-1], samples_used[-1])
 
 print('\n\n', scores)
+print(samples_used)
 print(np.mean(scores))
--- mondrian_forest_classifier_with_al_strategy.py
+++ mondrian_forest_classifier_with_al_strategy.py
@@ -28,8 +28,12 @@ class MondrianForestClassifierWithALStrategy(MondrianForestClassifier):
         # Retrieve the yet unused data samples
         remaining_idxs = [idx for idx in range(X.shape[0]) if idx not in inital_idxs]
 
+        samples_used = inital_dataset_size
         # Loop over all unused data samples and learn from them when the confidence is to low
         for sample_data, sample_target in zip(X[remaining_idxs], Y[remaining_idxs]):
             probabilities = self.predict_proba([sample_data])
             if self.calculate_confidence(probabilities[0]) < threshold:
                 self.partial_fit([sample_data], [sample_target])
+                samples_used += 1
+
+        return samples_used
