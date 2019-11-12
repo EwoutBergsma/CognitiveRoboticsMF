1e3eb6c39cc7bce9b77fe9dfc2ed0f3d230227ac
Sohyo <cienakim12@gmail.com>
2019-11-12 17:44:31 +0100

    integrate mondrian_forest_classifier_with_al_strategy

--- mondrian_forest_classifier_with_al_strategy.py
+++ mondrian_forest_classifier_with_al_strategy.py
@@ -13,25 +13,27 @@ class MondrianForestClassifierWithALStrategy(MondrianForestClassifier):
         return 1 - sorted_proba[1] / sorted_proba[0]
 
     @staticmethod
+    def shuffling(X_train, y_train):
+        shuffled_rows = np.random.permutation(X_train.shape[0])
+        return X_train[shuffled_rows], y_train[shuffled_rows]
+
+    @staticmethod
     def random_sampling(X_train, y_train, num_of_dataset):
         rand_selected_rows = np.random.choice(X_train.shape[0], num_of_dataset, replace=False)
         return X_train[rand_selected_rows, :], y_train[rand_selected_rows], rand_selected_rows
 
     def fit_using_al_strategy_thres(self, X, Y, classes=None, inital_dataset_size=300, threshold=0.5):
         classes = np.array(range(51)) if classes is None else classes  # default classes are 0-50
-
-        # first, get the initial data set
-        X_initial, Y_initial, inital_idxs = self.random_sampling(X, Y, inital_dataset_size)
+        # first shuffle the dataset and get the initial data
+        X, Y = self.shuffling(X, Y)
         # Partial fit on the initial data
-        self.partial_fit(X_initial, Y_initial, classes)
-
-        # Retrieve the yet unused data samples
-        remaining_idxs = [idx for idx in range(X.shape[0]) if idx not in inital_idxs]
+        self.partial_fit(X[:inital_dataset_size], Y[:inital_dataset_size], classes)
 
         # Retrieve the probability distributions for the remaining training samples
-        probabilities = self.predict_proba(X[remaining_idxs])
+        probabilities = self.predict_proba(X[inital_dataset_size:])
         # Filter the training samples based on the confidence of the mf
-        selected_idxs = [idx for probs, idx in zip(probabilities, remaining_idxs) if
+        print(len(probabilities), len(list(range(inital_dataset_size, X.shape[0]))))
+        selected_idxs = [idx for probs, idx in zip(probabilities, range(inital_dataset_size, X.shape[0])) if
                          self.calculate_confidence(probs) < threshold]
         # Fit again on the samples the mf is unsure about
         self.partial_fit(X[selected_idxs], Y[selected_idxs])
