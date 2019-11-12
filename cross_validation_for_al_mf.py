55ad792abcb77c0c1e592e52a1bec360672407e7
gitaar9 <thijs.eker@gmail.com>
2019-11-12 16:05:02 +0100

    corrected the import statement

--- cross_validation_for_al_mf.py
+++ cross_validation_for_al_mf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from load_dataset import load_vfh_data
-from mondrianForest.cleaned_up_active_learning import MondrianForestClassifierWithALStrategy
+from mondrian_forest_classifier_with_al_strategy import MondrianForestClassifierWithALStrategy
 
 data, labels, cv_generator = load_vfh_data()
 
@@ -11,7 +11,7 @@ num_trees_depthdata = 22
 
 scores = []
 for training_idxs, validation_idxs in cv_generator:
-    mf = MondrianForestClassifierWithALStrategy(n_estimators=22)
+    mf = MondrianForestClassifierWithALStrategy(n_estimators=10)
     mf.fit_using_al_strategy_thres(data[training_idxs], labels[training_idxs], list(range(51)), 300, 0.5)
     scores.append(mf.score(data[validation_idxs], labels[validation_idxs]))
 
