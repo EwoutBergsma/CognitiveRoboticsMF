83056a97be869d05fcda488b39e8adc9b5fc6798
gitaar9 <thijs.eker@gmail.com>
2019-11-12 19:59:30 +0100

    lower threshold

--- mrmr_feature_selection.py
+++ mrmr_feature_selection.py
@@ -1,9 +1,9 @@
 from skfeature.function.information_theoretical_based.MRMR import mrmr
 
-from load_dataset import load_all_image_feature_data
+from load_dataset import load_vfh_data
 
-data, labels, cv_generator = load_all_image_feature_data()
+data, labels, cv_generator = load_vfh_data()
 
 for _, split_idx in cv_generator:
-    selected_features, _, _ = mrmr(data[split_idx], labels[split_idx], n_selected_features=512)
+    selected_features, _, _ = mrmr(data[split_idx], labels[split_idx], n_selected_features=304//2)
     print(selected_features)
