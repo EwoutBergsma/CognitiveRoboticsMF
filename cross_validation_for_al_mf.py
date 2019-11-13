50a686e259dc6befd6256db667b4015688502190
T.A.Eker@student.rug.nl <s2576597@pg-interactive.hpc.rug.nl>
2019-11-13 17:46:06 +0100

    plz accept this

--- cross_validation_for_al_mf.py
+++ cross_validation_for_al_mf.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 from load_dataset import load_vfh_data
 from mondrian_forest_classifier_with_al_strategy import MondrianForestClassifierWithALStrategy
 import sys
@@ -15,7 +15,7 @@ scores = []
 samples_used = []
 
 print("\nPerforming cross-validation for vfh data with AL using a threshold of {}".format(threshold))
-
+start_time = time.time()
 for training_idxs, validation_idxs in cv_generator:
     mf = MondrianForestClassifierWithALStrategy(n_estimators=22)
     samples_used.append(mf.fit_using_al_strategy_thres(data[training_idxs], labels[training_idxs], np.array(range(51)),
@@ -24,6 +24,7 @@ for training_idxs, validation_idxs in cv_generator:
     print(scores[-1], samples_used[-1])
     del mf
 
+print("The cross validation took {} seconds".format(time.time() - start_time))
 print("Samples used: ", np.mean(scores), "+-", np.std(scores))
 print(samples_used)
 
