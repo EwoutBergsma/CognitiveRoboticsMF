01e584d7a1d65dfeca703254b7ccaecd98c3a3a7
T.A.Eker@student.rug.nl <s2576597@peregrine.hpc.rug.nl>
2019-11-13 15:33:44 +0100

    updated the mrmr script and the cross_validation to now accept a commandline argument

--- cross_validation_for_al_mf.py
+++ cross_validation_for_al_mf.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from load_dataset import load_vfh_data
 from mondrian_forest_classifier_with_al_strategy import MondrianForestClassifierWithALStrategy
-
+import sys
 
 ini_amount_depth_data_thres = 300
 ini_amount_depth_data_perc = 3000
@@ -10,16 +10,22 @@ num_trees_depth_data = 22
 
 data, labels, cv_generator = load_vfh_data()
 
+threshold = float(sys.argv[1])
 scores = []
 samples_used = []
+
+print("\nPerforming cross-validation for vfh data with AL using a threshold of {}".format(threshold))
+
 for training_idxs, validation_idxs in cv_generator:
     mf = MondrianForestClassifierWithALStrategy(n_estimators=22)
     samples_used.append(mf.fit_using_al_strategy_thres(data[training_idxs], labels[training_idxs], np.array(range(51)),
-                                                       300, 0.3))
+                                                       300, threshold))
     scores.append(mf.score(np.array(data[validation_idxs, :]), np.array(labels[validation_idxs])))
     print(scores[-1], samples_used[-1])
     del mf
 
-print('\n\n', scores)
+print("Samples used: ", np.mean(scores), "+-", np.std(scores))
 print(samples_used)
-print(np.mean(scores))
+
+print("Accuracies: ", np.mean(samples_used), "+-", np.std(samples_used))
+print(scores)
--- mrmr_feature_selection.py
+++ mrmr_feature_selection.py
@@ -1,9 +1,13 @@
 from skfeature.function.information_theoretical_based.MRMR import mrmr
 
-from load_dataset import load_vfh_data
+from load_dataset import load_all_image_feature_data
 
-data, labels, cv_generator = load_vfh_data()
+data, labels, cv_generator = load_all_image_feature_data()
 
-for _, split_idx in cv_generator:
-    selected_features, _, _ = mrmr(data[split_idx], labels[split_idx], n_selected_features=304//2)
+use_validation = True
+n_features = 512
+for training_idxs, validation_idxs in cv_generator:
+    split_idx = validation_idxs if use_validation else training_idxs
+    print("Running mRMR for image features on {} examples, selecting {} features.".format(len(split_idx), n_features))
+    selected_features, _, _ = mrmr(data[split_idx], labels[split_idx], n_selected_features=n_features)
     print(selected_features)
